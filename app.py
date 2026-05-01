9a9a3c54e34e5a39e9f43f50f0e2a4a1672109c8
Bhuvaneshwari Vijay Raghavan <bhuvaneshwari.vijay@gmail.com>
2026-05-01 20:27:47 +0530

    Update model name in chat completion request

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ Format:
 
             try:
                 response = client.chat.completions.create(
-                    model="llama3-8b-8192",
+                    model="llama-3.3-70b-versatile",
                     messages=[{"role": "user", "content": prompt}],
                     max_tokens=1000
                 )
